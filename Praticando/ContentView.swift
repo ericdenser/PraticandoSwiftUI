851b2d2b5e85d3c571f2550a05f05950ec5ea6ff
ericdenser <ericdenser7@gmail.com>
2025-07-27 19:31:06 -0300

    Exercicios finalizados

--- Praticando/ContentView.swift
+++ Praticando/ContentView.swift
@@ -1,21 +1,106 @@
 //
 //  ContentView.swift
-//  Praticando
+//  Praticando exercicios do curso TIC EM TRILHAS
 //
 //  Created by Eric on 27/07/25.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct TelaFormas: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.pink)
+                .frame(width: 200, height: 200)
+                
+            HStack(spacing: 30) {
+                VStack(spacing: 20) {
+                    Rectangle()
+                        .fill(Color.yellow)
+                        .frame(width: 70, height: 70)
+                    
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 70, height: 70)
+                    
+                }
+                Capsule()
+                    .fill(Color.green)
+                    .frame(width: 60, height: 150)
+            }
+        }
+    }
+}
+
+struct TelaContador: View {
+    @State var contador: Int = 0
+    
+    var body: some View {
+        VStack(spacing: 70) {
+            Text("\(contador)")
+                .bold()
+                .font(.largeTitle)
+                .foregroundStyle(.pink)
+            Button {
+                contador += 1
+            } label: {
+                Text("Incrementa")
+                    .font(.headline)
+                    .bold()
+                    .padding()
+                    .frame(width: 150, height: 50)
+                    .background(Color.pink)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+                
+                
+            }
+        }
+    }
+}
+
+struct TelaPersonagem: View {
+    var TextoSobreRick: String = "Protagonista da série Rick And Morty, velho bêbado e engracado, homem mais inteligente do universo."
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("Rick")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.red, lineWidth: 4))
+            
+            Text("Rick")
+                .font(.largeTitle)
+                .padding(.top, 10)
+                .bold()
+            
+            Text(TextoSobreRick)
+                .font(.body)
+                .padding()
+                .multilineTextAlignment(.center)
+            
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            TelaFormas()
+                .tabItem {
+                    Label("Shapes", systemImage: "square.fill.on.square.fill")
+                }
+            TelaContador()
+                .tabItem {
+                    Label("Counter", systemImage: "plus.circle.fill")
+                }
+            TelaPersonagem()
+                .tabItem {
+                    Label("Character", systemImage: "person.fill")
+                }
+        }
     }
 }
 
